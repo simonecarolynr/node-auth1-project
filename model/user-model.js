9bea8d0545952d49ad9a4481e7f5f8527e3c4eab
Simone Roy <simonecarolynr@gmail.com>
2020-09-02 00:02:03 -0600

    router and model files ready

---
+++ model/user-model.js
@@ -0,0 +1,22 @@
+const db = require('../data/dbConfig')
+
+// Add a user and hash the password
+function addUser(){
+
+}
+
+// Log in the user, authenticate the credentials
+function loginUser(){
+
+}
+
+// Get all users, with authorization
+function getAllUsers(){
+
+}
+
+module.exports = {
+    addUser,
+    loginUser,
+    getAllUsers
+}
---
+++ router/user-router.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const db = require('../model/user-model')
+
+const user = express.Router()
+
+//CREATE a user '/api/register'
+
+
+//CREATE a new session upon authenticating the user
+
+
+//RETRIEVE the users, if authorized
