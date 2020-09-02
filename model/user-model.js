1a56aaef0077fe6801fc389efacc1b1a6836168c
Simone Roy <simonecarolynr@gmail.com>
2020-09-02 00:19:41 -0600

    models done

--- model/user-model.js
+++ model/user-model.js
@@ -1,18 +1,22 @@
 const db = require('../data/dbConfig')
 
 // Add a user and hash the password
-function addUser(){
-    
+function addUser(userCredentials){
+    return db("users")
+        .insert(userCredentials)
 }
 
 // Log in the user, authenticate the credentials
-function loginUser(){
-
+function loginUser(filter){
+    return db("users")
+        .select("id", "username", "password")
+        .where(filter)
 }
 
 // Get all users, with authorization
 function getAllUsers(){
-
+    return db("users")
+        .select("id", "username")
 }
 
 module.exports = {
