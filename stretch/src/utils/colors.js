883094eb64d6f9ef88fd93597b66b9bb0f4346ed
Simone Roy <simonecarolynr@gmail.com>
2020-09-02 19:21:46 -0600

    stretch attempted

---
+++ stretch/src/utils/colors.js
@@ -0,0 +1,9 @@
+export const yellow = "#FBAF00";
+
+export const teal = "#223843";
+
+export const orange = "#F46036";
+
+export const black = "#011627";
+
+export const neutral = "#ECEBE4";
---
+++ stretch/src/utils/typography.js
@@ -0,0 +1,9 @@
+export const primaryFont = "'Roboto Mono', monospace";
+
+export const typeScale = {
+    header1: "3.052em",
+    header2: "4.441em",
+    header3: "1.953em",
+    paragraph: "1.263em",
+    copyrightText: "0.64em"
+}
